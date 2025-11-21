92c3244691e2201fcfa2d8a5a8aa44b8f7a007fe
Santiago Barrera <sanseb290514@gmail.com>
2025-11-21 14:43:04 -0500

    feat(auth): Implemented guard to protect privates endpoints

--- environment.d.ts
+++ environment.d.ts
@@ -2,6 +2,11 @@ declare global {
 	namespace NodeJS {
 		interface ProcessEnv {
 			JWT_SECRET: string;
+			DATABASE_HOST: string;
+			DATABASE_PORT: number;
+			DATABASE_USERNAME: string;
+			DATABASE_PASSWORD: string;
+			DATABASE_NAME: string;
 		}
 	}
 }
